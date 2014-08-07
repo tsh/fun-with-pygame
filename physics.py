ba88ed44029e7a2691e0041d0a4a46fe25af1022
Anton Nikulin <dr.tallin@gmail.com>
2014-08-07 23:01:36 +0300

    physics: circle class

--- physics.py
+++ physics.py
@@ -4,12 +4,27 @@ http://www.petercollingridge.co.uk/pygame-physics-simulation
 """
 import pygame
 
+
+class Particle:
+    def __init__(self, (x, y), size):
+        self.x = x
+        self.y = y
+        self.size = size
+        self.colour = (0, 0, 255)
+        self.thickness = 1
+
+    def display(self):
+        pygame.draw.circle(screen, self.colour, (self.x, self.y), self.size, self.thickness)
+
 (width, height) = (300, 200)
 background_colour = (255,255,255)
 
 screen = pygame.display.set_mode((width, height))
 screen.fill(background_colour)
-pygame.display.set_caption('Tutorial 1')
+pygame.display.set_caption('Ph sim')
+
+my_first_particle = Particle((150, 50), 15)
+my_first_particle.display()
 
 pygame.display.flip()
 
