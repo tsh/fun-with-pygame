dad63dca6fed71b01dedacd9e98a36296f18364c
Anton Nikulin <dr.tallin@gmail.com>
2014-08-10 15:55:51 +0300

    physics: 6 gravity

--- physics.py
+++ physics.py
@@ -11,6 +11,20 @@ import pygame
 background_colour = (255,255,255)
 (width, height) = (400, 400)
 
+# Sim constants
+gravity = (math.pi, 0.002)
+number_of_particles = 1
+drag = 0.999  # air resistance - the faster a particle is moving, the more speed is lost.
+elasticity = 0.75 # loss of speed a particle experiences when it hits a boundary.
+
+def addVectors((angle1, length1), (angle2, length2)):
+    """ Takes two vectors (each an angle and a length), and returns single, combined a vector."""
+    x = math.sin(angle1) * length1 + math.sin(angle2) * length2
+    y = math.cos(angle1) * length1 + math.cos(angle2) * length2
+    length = math.hypot(x, y)
+    angle = 0.5 * math.pi - math.atan2(y, x)
+    return angle, length
+
 
 class Particle():
     def __init__(self, (x, y), size):
@@ -26,6 +40,8 @@ class Particle():
         pygame.draw.circle(screen, self.colour, (int(self.x), int(self.y)), self.size, self.thickness)
 
     def move(self):
+        (self.angle, self.speed) = addVectors((self.angle, self.speed), gravity)
+        self.speed *= drag
         self.x += math.sin(self.angle) * self.speed
         self.y -= math.cos(self.angle) * self.speed
 
@@ -40,27 +56,30 @@ class Particle():
         # self.x = (width - self.size) - d
         # This simplifies to:
         # self.x = 2*(width - self.size) - self.x
-        
+
         if self.x > width - self.size:
+            self.speed *= elasticity
             self.x = 2*(width - self.size) - self.x
             self.angle = - self.angle
 
         elif self.x < self.size:
+            self.speed *= elasticity
             self.x = 2*self.size - self.x
             self.angle = - self.angle
 
         if self.y > height - self.size:
+            self.speed *= elasticity
             self.y = 2*(height - self.size) - self.y
             self.angle = math.pi - self.angle
 
         elif self.y < self.size:
+            self.speed *= elasticity
             self.y = 2*self.size - self.y
             self.angle = math.pi - self.angle
 
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Tutorial 4')
+pygame.display.set_caption('Pygame')
 
-number_of_particles = 10
 my_particles = []
 
 for n in range(number_of_particles):
