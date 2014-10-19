8d3f1aa9d0b486a0310ce99ca1b94c584d708c9e
Anton Nikulin <dr.tallin@gmail.com>
2014-10-19 23:44:52 +0300

    some comment

--- personal/key_sprite_rotation.py
+++ personal/key_sprite_rotation.py
@@ -9,16 +9,16 @@ screen = pygame.display.set_mode((640, 480), 0, 32)
 class Ship(object):
     def __init__(self):
         self.main_image = pygame.image.load(os.path.join("..", "assets", "ship.jpg")).convert()
-        self.image = self.main_image
+        self.image = self.main_image  # we need copy for rotating sprite
         self.rect = self.image.get_rect()
         self.rect.center = (320, 240)
         self.dir = 0
 
     def update(self):
-        old_center = self.rect.center
-        self.image = pygame.transform.rotate(self.main_image, self.dir)
+        old_center = self.rect.center  # rotating distort original image and its coordinates
+        self.image = pygame.transform.rotate(self.main_image, self.dir)  # rotate one image, and render another otherwise `out of memory`
         self.rect = self.image.get_rect()
-        self.rect.center = old_center
+        self.rect.center = old_center  # set rotated image to previous coordinates
 
     def render(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
